5d7b275ad8d3b108c1056f2da1e8b47cc756b859
AlexJBz <alexjkenworthy@gmail.com>
2019-10-24 12:18:01 +0100

    Added canvas to body, did core css, auto-resize implemented, skeleton of game in progress

--- js/main.js
+++ js/main.js
@@ -1,1 +1,42 @@
-alert('test');
+let game = {
+    pixi: new PIXI.Application({
+        width: window.innerWidth,
+        height: window.innerHeight,
+        backgroundColor: 0x740574,
+        antialias: false,
+        resolution: 1
+    }),
+    settings: {
+        width: 50,
+        length: 50,
+        tileSize: 10,
+        snakeSize: 8
+    },
+    mapClass: class Map extends PIXI.Graphics {
+        constructor(width, length) {
+
+        }
+
+        render () {
+
+        }
+    },
+    tileClass: class Tile extends PIXI.Graphics {
+        constructor(startX, startY) {
+
+        }
+    },
+    map: null,
+    positioning () {
+        window.addEventListener('resize', ()=> {
+            this.pixi.renderer.resize(window.innerWidth, window.innerHeight);
+            this.pixi.stage.position.set(window.innerWidth / 2, window.innerHeight / 2);
+        });
+    },
+    init () {
+        document.body.appendChild(this.pixi.renderer.view);
+        this.positioning();
+    }
+}
+
+game.init();
