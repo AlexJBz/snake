44a847051f2c7fc2161db84c4f19c5bd07580c85
AlexJBz <alexjkenworthy@gmail.com>
2019-10-24 16:25:38 +0100

    Tiles are generated

--- js/main.js
+++ js/main.js
@@ -6,36 +6,68 @@ let game = {
         antialias: false,
         resolution: 1
     }),
+    colours: {
+        white: 0xFFFFFF,
+        black: 0x000000
+    },
     settings: {
         width: 50,
-        length: 50,
-        tileSize: 10,
+        length: 40,
+        tileSize: 15,
         snakeSize: 8
     },
     mapClass: class Map extends PIXI.Graphics {
         constructor(width, length) {
-
+            super();
+            this.xWidth = width;
+            this.yLength = length;
+            this.draw();
+            this.position.set(-this.width / 2, -this.height / 2);
         }
 
-        render () {
-
+        draw () {
+            for (let x = 0; x < this.xWidth; x++) {
+                for (let y = 0; y < this.yLength; y++) {
+                    this.addChild(new game.tileClass());
+                    this.children[this.children.length - 1].position.set(x * game.settings.tileSize, y * game.settings.tileSize);
+                }
+            }
         }
     },
     tileClass: class Tile extends PIXI.Graphics {
         constructor(startX, startY) {
+            super();
+            this.startX = startX;
+            this.startY = startY;
+            this.draw();
+        }
 
+        draw () {
+            this.moveTo(0, 0)
+                .lineStyle(1, game.colours.black)
+                .beginFill(game.colours.white)
+                .lineTo(game.settings.tileSize, 0) 
+                .lineTo(game.settings.tileSize, game.settings.tileSize)
+                .lineTo(0, game.settings.tileSize)
+                .lineTo(0, 0)
         }
     },
     map: null,
     positioning () {
         window.addEventListener('resize', ()=> {
-            this.pixi.renderer.resize(window.innerWidth, window.innerHeight);
-            this.pixi.stage.position.set(window.innerWidth / 2, window.innerHeight / 2);
+            this.position();
         });
+    },
+    position() {
+        this.pixi.renderer.resize(window.innerWidth, window.innerHeight);
+        this.pixi.stage.position.set(window.innerWidth / 2, window.innerHeight / 2);
     },
     init () {
         document.body.appendChild(this.pixi.renderer.view);
+        this.position();
         this.positioning();
+        this.map = new this.mapClass(this.settings.width, this.settings.length);
+        this.pixi.stage.addChild(this.map);
     }
 }
 
