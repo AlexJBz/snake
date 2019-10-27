c33af2376fda22971851fe6c60d748503a385f07
AlexJBz <alexjkenworthy@gmail.com>
2019-10-27 20:01:17 +0000

    Added a snake that can move

--- js/main.js
+++ js/main.js
@@ -8,13 +8,13 @@ let game = {
     }),
     colours: {
         white: 0xFFFFFF,
-        black: 0x000000
+        black: 0x000000,
+        red: 0xCC0E00
     },
     settings: {
         width: 50,
         length: 40,
         tileSize: 15,
-        snakeSize: 8
     },
     mapClass: class Map extends PIXI.Graphics {
         constructor(width, length) {
@@ -52,7 +52,75 @@ let game = {
                 .lineTo(0, 0)
         }
     },
+    snakeClass: class Snake extends PIXI.Graphics {
+        constructor() {
+            super();
+            this.parts = [ { x: 0, y: 0 }, { x: 0, y: 0 }, { x: 0, y: 0 }, { x: 0, y: 0 }, { x: 0, y: 0 }, { x: 0, y: 0 }, { x: 0, y: 0 }, { x: 0, y: 0 } ];
+            this.moveX = 0;
+            this.moveY = 0;
+            this.draw();
+        }
+
+        draw () {
+            this.parts.forEach(part => {
+                this.lineStyle(1, game.colours.red)
+                    .beginFill(game.colours.red)
+                    .drawRect(part.x * game.settings.tileSize + 1, part.y * game.settings.tileSize + 1, game.settings.tileSize - 2, game.settings.tileSize - 2)
+                    .endFill();
+            });
+        }
+        
+        move () {
+            let head = this.parts[0];
+            let tail = this.parts.pop();
+            tail.x = head.x + this.moveX;
+            tail.y = head.y + this.moveY;
+            this.parts.unshift(tail);
+            if (this.checkCollisions()) {
+                this.clear();
+                this.draw();
+            } else {
+                console.log('Game over!');
+            }
+        }
+
+        checkCollisions () {
+            let head = this.parts[0];
+            if (head.x > game.settings.width || head.y > game.settings.length || head.x < 0 || head.y < 0) {
+                clearInterval(game.loop);
+                return false;
+            }
+            return true;
+        }
+
+        changeDirection(keyCode) {
+            switch(keyCode) {
+                case 37:
+                    this.moveX = -1;
+                    this.moveY = 0;
+                    break;
+                case 38:
+                    this.moveX = 0;
+                    this.moveY = -1;
+                    break;
+                case 39:
+                    this.moveX = 1;
+                    this.moveY = 0;
+                    break;
+                case 40:
+                    this.moveX = 0;
+                    this.moveY = 1;
+                    break;
+                default:
+                    // Oi stop playing with the console
+                    console.log('Cheeky!')
+                    break;
+            }
+        }
+    },
     map: null,
+    snake: null,
+    loop: null,
     positioning () {
         window.addEventListener('resize', ()=> {
             this.position();
@@ -68,6 +136,18 @@ let game = {
         this.positioning();
         this.map = new this.mapClass(this.settings.width, this.settings.length);
         this.pixi.stage.addChild(this.map);
+        this.snake = new this.snakeClass();
+        this.map.addChild(this.snake);
+
+        window.addEventListener('keyup', (e)=> {
+            if (e.keyCode >= 37 && e.keyCode <= 40) {
+                this.snake.changeDirection(e.keyCode);
+            }
+        });
+
+        game.loop = setInterval(()=> {
+            game.snake.move();
+        }, 150);
     }
 }
 
