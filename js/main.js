568fd26d04970476d5752d6d2024e3e4e7ee4fb9
AlexJBz <alexjkenworthy@gmail.com>
2019-10-28 09:39:20 +0000

    Broken snake

--- js/main.js
+++ js/main.js
@@ -13,8 +13,7 @@ let game = {
     settings: {
         width: 50,
         length: 40,
-        tileSize: 15,
-        snakeSize: 8
+        tileSize: 15
     },
     mapClass: class Map extends PIXI.Graphics {
         constructor(width, length) {
@@ -52,7 +51,62 @@ let game = {
                 .lineTo(0, 0)
         }
     },
+    snakeClass: class Snake extends PIXI.Graphics {
+        constructor() {
+            super();
+            this.length = 0;
+            this.addLength();
+        }
+
+        addLength () {
+            this.length++;
+            this.addChild(new game.partClass());
+        }
+
+        move (key) {
+            switch (key) {
+                case 37:
+                    console.log('left');
+                    break;
+                case 38:
+                    console.log('up');
+                    break;
+                case 39:
+                    console.log('right');
+                    break;
+                case 40:
+                    console.log('down');
+                    break;
+            }
+        }
+    },
+    partClass: class Part extends PIXI.Graphics {
+        constructor() {
+            super();
+            this.draw();
+        }
+
+        draw () {
+            this.moveTo(-1, 0)
+                .beginFill(0xFF0000)
+                .lineStyle(1, 0xFf0000)
+                .lineTo(game.settings.tileSize - 1, 0)
+                .lineTo(game.settings.tileSize - 1, game.settings.tileSize)
+                .lineTo(-1, game.settings.tileSize)
+                .lineTo(-1, 0)
+                .endFill();
+        }
+
+        move (x, y) {
+            let pixelX = x * game.settings.tileSize;
+            let pixelY = y * game.settings.tileSize;
+            this.position.x += pixelX;
+            this.position.y += pixelY;
+        }
+
+    },
     map: null,
+    snake: null,
     positioning () {
         window.addEventListener('resize', ()=> {
             this.position();
@@ -67,7 +121,15 @@ let game = {
         this.position();
         this.positioning();
         this.map = new this.mapClass(this.settings.width, this.settings.length);
+        this.snake = new this.snakeClass();
         this.pixi.stage.addChild(this.map);
+        this.pixi.stage.addChild(this.snake);
+        window.addEventListener('keyup', (e)=> {
+            let key = e.keyCode;
+            if (key >= 37 && key <= 40) {
+                this.snake.move(key);
+            }
+        });
     }
 }
 
